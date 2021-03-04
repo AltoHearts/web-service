71abd0cb2afe7684364aa3e93e893e27fd84ce23
AltoHearts <72461104+AltoHearts@users.noreply.github.com>
2021-03-03 18:20:22 -0800

    adds Cirque du Freak js

---
+++ data/cirque-du-freak.js
@@ -0,0 +1,34 @@
+{
+	"title": "Bond Films by Box Office Totals",
+	
+	"films":[
+             {
+				"Film":23,
+				"Title":"mine",
+				"Year":2012,
+				"Director":"Sam Mendes",
+				"Producers":"Michael G. Wilson and Barbara Broccoli",
+				"Writers":"Neal Purvis, Robert Wade and John Logan",
+				"Composer":"Thomas Newman",
+				"Bond":"Daniel Craig",
+				"Budget":"$230,000,000.00",
+				"BoxOffice":"$1,110,526,981.00",
+				"Image":"skyfall.jpg"
+			  },
+              {
+				"Film":24,
+				"Title":"Spectre",
+				"Year":2015,
+				"Director":"Sam Mendes",
+				"Producers":"Michael G. Wilson and Barbara Broccoli",
+				"Writers":"Neal Purvis, Robert Wade and John Logan",
+				"Composer":"Thomas Newman",
+				"Bond":"Daniel Craig",
+				"Budget":"$200,000,000.00",
+				"BoxOffice":"$879,620,923.00",
+				"Image":"spectre.jpg"
+			  },
+    
+
+			]
+}
