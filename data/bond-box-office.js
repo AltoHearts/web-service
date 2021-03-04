03e2348281b011440b7e3e3bb698e1cbd2b6beda
AltoHearts <72461104+AltoHearts@users.noreply.github.com>
2021-03-03 19:22:49 -0800

    adds bookibois

--- data/bond-box-office.js
+++ data/bond-box-office.js
@@ -1,15 +1,106 @@
 {
 	"title": "Bond Films by Box Office Totals",
 	
-	"films":[
+	"books":[
 				{
 				"Book":1, 
 				"Title":"A Living Nightmare", 
 				"Year":2002, 
 				"Author":"Darren Shan", 
-				"Rating:"4.5/5 Stars", 
+				"Rating":"4.5/5 Stars", 
 				"Image":"alivingnightmare.jpg" 
+				},
+				{
+				"Book":2, 
+				"Title":"The Vampire’s Assistant", 
+				"Year":2002, 
+				"Author":"Darren Shan", 
+				"Rating":"4.5/5 Stars", 
+				"Image":"vampassist.jpg" 
+				},
+				{
+				"Book":3, 
+				"Title":"Tunnels of Blood", 
+				"Year":2003, 
+				"Author":"Darren Shan", 
+				"Rating":"4.7/5 Stars", 
+				"Image":"tunnelsblood.jpg" 
+				},
+				{"Book":4,
+				"Title":"Vampire Mountain",
+				"Year":2003, 
+				"Author":"Darren Shan", 
+				"Rating":"4.8/5 Stars", 
+				"Image":"vampiremountain.jpg"
+				},
+				{
+				"Book":5, 
+				"Title":"Trails of Death", 
+				"Year":2004, 
+				"Author":"Darren Shan", 
+				"Rating":"4.7/5 Stars", 
+				"Image":"trailsodeath.jpg"
+				},
+				{
+				"Book":6, 
+				"Title":"The Vampire Prince", 
+				"Year":2004, 
+				"Author":"Darren Shan", 
+				"Rating":"4.7/5 Stars", 
+				"Image":"vampprince.jpg"
+				},
+				{
+				"Book":7,
+				"Title":"Hunters of the Dusk",
+				"Year":2005, 
+				"Author":"Darren Shan", 
+				"Rating":"4.7/5 Stars", 
+				"Image":"huntersofdusk.jpg"
+				},
+				{
+				"Book":8, 
+				"Title":"Allies of the Night", 
+				"Year":2005, 
+				"Author":"Darren Shan", 
+				"Rating":"4.7/5 Stars", 
+				"Image":"alliesofthenight.jpg"
+				},
+				{
+				"Book":9, 
+				"Title":"Killers of the Dawn", 
+				"Year":2003, 
+				"Author":"Darren Shan", 
+				"Rating":"4.7/5 Stars", 
+				"Image":"killersdawn.jpg"
+				},
+				{
+				"Book":10, 
+				"Title":"The Lake of Souls", 
+				"Year":2009, 
+				"Author":"Darren Shan", 
+				"Rating":"4.7/5 Stars", 
+				"Image":"lakeofsouls.jpg"
+				},
+				{
+				"Book":11, 
+				"Title":"Lord of the Shadows", 
+				"Year":2007, 
+				"Author":"Darren Shan", 
+				"Rating":"4.8/5 Stars", 
+				"Image":"lordoshadow.jpg"
+				},
+				{"Book":12, 
+				"Title":"Sons of Destiny", 
+				"Year":2003, 
+				"Author":"Darren Shan", 
+				"Rating":"4.7/5 Stars", 
+				"Image":"sonsofdestiny.jpg"
 				}
+
+ 
+
+ 
+
              
 
 			]
